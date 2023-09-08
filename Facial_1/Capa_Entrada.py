2ea4f78e526c5baf6bd8d1a30a27d45afee86ef0
Gian Severiche <gianseveriche97@gmail.com>
2023-09-08 12:48:14 -0500

    Se realizo un ajuste en la ubicacion del archivo que elimina el ruido de la imagen

--- Facial_1/Capa_Entrada.py
+++ Facial_1/Capa_Entrada.py
@@ -1,13 +1,13 @@
 import cv2 as cv
 import numpy as np
-ruido = cv.CascadeClassifier('D:\Clases\Proyectos_Python\opencv-4.x\data\haarcascades\haarcascade_frontalface_default.xml')
+ruido = cv.CascadeClassifier('Cv\haarcascade_frontalface_default.xml')
 camara = cv.VideoCapture(1)
 while True:
     _, captura = camara.read()
     gris = cv.cvtColor(captura, cv.COLOR_BGR2GRAY)
     rostro = ruido.detectMultiScale(gris, 1.3, 3)
     for(x, y, e1, e2) in rostro:
-        cv.rectangle(captura, (x,y), (x+e1,y+e2), (255,0,0), 2)
+        cv.rectangle(captura, (x,y), (x+e1,y+e2), (0,0,255), 2)
 
     cv.imshow("Resultado", captura)
     
